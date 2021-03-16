1ba44bc2a78e9fa56f46d1788f409ee811415829
flippedbit <straughan.michael@gmail.com>
2021-03-16 13:19:51 -0400

    day3 js

---
+++ 10_Days_of_Javascript/3-Arrays.js
@@ -0,0 +1,54 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    var result;
+    var tmp = [];
+    for (let i of nums) {
+        if (!tmp.includes(i)) {
+            tmp.push(i);
+        }
+    }
+    tmp.sort(function(a,b){return a - b});
+    if (tmp.length > 2) {
+        result = tmp[tmp.length - 2]
+    } else {
+        result = tmp[0]
+    }
+    return result;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
