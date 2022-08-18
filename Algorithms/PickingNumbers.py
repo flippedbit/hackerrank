991efc4e0ec01be72d4698b32d054fc4e786a7e6
flippedbit <straughan.michael@gmail.com>
2022-08-18 14:34:58 -0400

    add init Picking Numbers algorithm problem

---
+++ Algorithms/PickingNumbers.py
@@ -0,0 +1,70 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to 1.
+#
+# Example:
+# a = [1,1,2,2,4,4,5,5,5]
+# There are two subarrays meeting the criterion: [1,1,2,2] and [4,4,5,5,5]. The maximum length subarray is 5 elements.
+#
+# Sample Input 0:
+# 6
+# 4 6 5 3 3 1
+# Sample Output 0:
+# 3
+# Sample Input 1:
+# 6
+# 1 2 2 3 1 2
+# Sample Output 1:
+# 5
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    # Write your code here
+    # base case: array only has 1 element
+    if len(a) == 1:
+        return 1
+    
+    # base case: all elements are the same
+    temp = list(set(a))
+    if len(temp) == 1:
+        return len(a)
+
+    max = 0
+    for i in range(len(a)):
+        count = 0
+        for j in range(len(a)):
+            if i == j:
+                pass
+            else:
+                if abs(a[i] - a[j]) <= 1:
+                    count = count + 1
+
+        if count > max:
+            max = count
+
+    return max
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+    print(result)
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
